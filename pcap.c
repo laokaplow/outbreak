216bf8a298caaf47535ef23c5f2c9aca5cba4b43
austenlauria <alauria@mail.csuchico.edu>
2014-08-29 01:47:46 -0700

    Minor changes to pcap.c

--- pcap.c
+++ pcap.c
@@ -7,11 +7,11 @@ int main()
 {
 	
   pcap_t *handle; 
-  char *device;
-  struct bpf_program filter_program;
-  char filter_expression[] = "tcp";
-  bpf_u_int32 mask;
-  bpf_u_int32 net;
+  char *device = NULL;
+  struct bpf_program filter_program;    /* Currently Unused */
+  char filter_expression[] = "tcp"; 	/* Currently Unused */
+  bpf_u_int32 mask;			/* Currently Unused */
+  bpf_u_int32 net;                      /* Currently Unused */
   struct pcap_pkthdr packet_header;
   const u_char *packet;  
   char errbuf[100];  
@@ -26,29 +26,37 @@ int main()
 
   if(pcap_findalldevs(&all_devices, errbuf) < 0) {
     printf("Error in findalldevs: %s\n", errbuf);
+    return;
   }
 
   for(d = all_devices; d; d = d->next) {
  //  printf("%d  %s\n", ++i, d->name);
    if(strcmp(d->name, "any") == 0) {
-     printf("Found any\n");
+    // printf("Found any\n");
      device = d->name;
    }  
   }
+
+  if(device == NULL) {
+    printf("No device found!\n");
+    return;
+  } 
   
   if((handle = pcap_open_live(device, 100, 1, 1000, errbuf)) == NULL) {
-    printf("error in open_live %s\n", errbuf);
+    printf("Error in pcap_open_live() %s\n", errbuf);
     return;
   }
   
-  packet = pcap_next(handle, &packet_header);
+ /* packet = pcap_next(handle, &packet_header);
   printf("Here's packet length: %d\n", packet_header.len);
-  
+ */
+ 
   if((dump_file = pcap_dump_open(handle, "./file.pcap")) == NULL) {
     printf("Error opening dump file!\n");
     return;  
   }
  
+  /* Calls packet_handler sub routine to dump packets to file */
   pcap_loop(handle, 0, packet_handler, (unsigned char *) dump_file);
 
   return 0;
