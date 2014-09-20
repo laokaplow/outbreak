b3a164dc7b359d5474ebf188057f6450d0eff5ec
zysh <hi.imma.bear@gmail.com>
2014-09-20 15:45:19 -0700

    Added a basic traceroute API

--- application.py
+++ application.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask.ext import restful
+
+from api.traceroute import Traceroute
+
 app = Flask(__name__, static_folder="static")
+api = restful.Api(app)
 
 @app.route("/")
 def root():
     return render_template("outbreak.html") 
 
+api.add_resource(Traceroute, '/traceroute/<string:dest>')
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, threaded=True)
 
