1c648209c28d77b176e58e2376dc82f4d7d3c065
austenlauria <alauria@mail.csuchico.edu>
2014-09-27 20:07:41 -0700

    Fixed compile errors...

--- birdseye/src/pcap_test.py
+++ birdseye/src/pcap_test.py
@@ -5,7 +5,7 @@ import subprocess
 out = subprocess.Popen(["sudo", "./pcap"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 for line in iter(out.stdout.readline, ''):
-	print line.rstrip()
+       print line.rstrip()
 
 for line in iter(out.stderr.readline, ''):
-	print line.rstrip()
+       print line.rstrip()
