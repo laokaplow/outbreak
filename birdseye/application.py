d0b697cd66e9b46baf8d6f591407b13b9bdb5d2a
zysh <hi.imma.bear@gmail.com>
2014-10-14 16:44:07 -0700

    Added image route

--- birdseye/application.py
+++ birdseye/application.py
@@ -36,6 +36,11 @@ def static_lib(filename):
     """ Static file path for external libraries """
     return send_from_directory(app.root_path + '/lib/bower_components/', filename)
 
+@app.route('/static/images/<path:filename>')
+def static_img(filename):
+    """ Static file path for images """
+    return send_from_directory(app.root_path + '/static/images/', filename)
+
 
 class Traceroute(restful.Resource):
     def get(self, dest):
