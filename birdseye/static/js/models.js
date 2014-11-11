9caca566116a53db61f09180d9fe52dc7b698cd4
zysh <hi.imma.bear@gmail.com>
2014-11-11 02:33:56 -0800

    Backbone modules created, added route highlighting

--- birdseye/static/js/models.js
+++ birdseye/static/js/models.js
@@ -2,31 +2,111 @@
  * @file: models.js
  */
 
-
 var Endpoint = Backbone.Model.extend({
   defaults: {
+    city: "",
+    country: "",
+    ip: "",
     lat: 0.0,
     lon: 0.0,
-    ip: ""
+    loss: "",
+    ping: "",
+    region: ""
+  },
+
+  initialize: function () {
   }
 });
 
+
+var Traceroute = Backbone.Collection.extend({
+  model: Endpoint,
+
+  initialize: function (options) {
+    this.id = options.id;
+    this.color = options.color;
+    this.markers = []
+    this.path = null;
+    this.highlight = null;
+
+    _.bindAll(this, "drawRoute");
+    _.bindAll(this, "showHighlight");
+    _.bindAll(this, "hideHighlight");
+  },
+
+  url: function () {
+    return "/traceroute/" + this.id;
+  },
+
+  parse: function (resp) {
+    return resp.destinations;
+  },
+
+  drawRoute: function (map) {
+    var self = this;
+    var coords = [];
+
+    this.each(function (obj) {
+      var a = obj.attributes;
+      var c = new google.maps.LatLng(a.lat, a.lon);
+
+      self.markers.push(new google.maps.Marker({
+        position: c,
+        map: map,
+        title: obj.cid,
+        color: self.color
+      }));
+
+      coords.push(c);
+    });
+
+    this.path = new google.maps.Polyline({
+        path: coords,
+        geodesic: true,
+        strokeColor: self.color,
+        strokeOpacity: 1.0,
+        strokeWeight: 3,
+        optimized: false,
+        zIndex: 50
+    });
+
+    this.highlight = new google.maps.Polyline({
+        path: coords,
+        geodesic: true,
+        strokeColor: self.color,
+        strokeOpacity: 0.3,
+        strokeWeight: 7,
+        optimized: false,
+        zIndex: 50
+    });
+
+    this.path.setMap(map);
+    google.maps.event.addListener(this.path, 'mouseover', function() {
+      self.showHighlight(map);
+    });
+
+    google.maps.event.addListener(this.path, 'mouseout', function() {
+      self.hideHighlight(map);
+    });
+  },
+
+  showHighlight: function (map) {
+    this.highlight.setMap(map);
+  },
+
+  hideHighlight: function (map) {
+    this.highlight.setMap(null);
+  }
+
+});
+
+
+/*
 var Pcap = Backbone.Model.extend({
   defaults: {
    packets: 0,
    filter: ""
   }
 });
+*/
 
-var Traceroute = Backbone.Collection.extend({
-  model: Endpoint,
-  initialize: function (models, options) {
-    this.id = options.id;
-  },
-  url: function () {
-    return "/traceroute/" + this.id;
-  },
-  parse: function (resp) {
-    return resp.destinations;
-  }
-});
--- birdseye/static/js/views.js
+++ birdseye/static/js/views.js
@@ -5,29 +5,127 @@
 
 var AppView = Backbone.View.extend({
   el: $('body'),
-  initialize: function () {
-    this.manualRoutesView = new ManualRoutesView;
-  }
-});
 
-/*
-var MonitoringView = Backbone.View.extend({
-  el: $('#monitoring_button'),
-  initialize: function () {
-    console.log("Here!");
+  events: {
+    "click #submit_button": "startTraceroute"
   },
-  render: function() {
+
+  initialize: function () {
+    this.traceroutes = [];
+    this.routeslistview = new RoutesListView;
+    this.googlemapView = new GoogleMapView;
+
+    _.bindAll(this, "startTraceroute");
+    _.bindAll(this, "drawTraceroute");
+  },
+
+  startTraceroute: function () {
+    var self = this;
+    var r = new Traceroute({
+      id: $("#search_field").val(),
+      color: randomColor()
+    });
+
+    r.fetch({
+      success: function () {
+        r.drawRoute(self.googlemapView.map);
+      },
+      fail: function () {
+        console.log("Traceroute failed");
+      }
+    });
+    this.traceroutes.push(r);
   }
-
-
 });
-*/
 
-var ManualRoutesView = Backbone.View.extend({
+
+var RoutesListView = Backbone.View.extend({
   el: $('#manual_routes'),
   initialize: function () {
-  },
-  render: function () {
   }
 });
 
+
+var GoogleMapView = Backbone.View.extend({
+  el: $('#map_canvas'),
+  initialize: function () {
+    this.featureOpts = [
+      {
+        "featureType": "water",
+        "stylers": [
+          { "hue": "#a7c5bd" },
+          { "lightness": -50},
+          { "saturation": -60}
+        ]
+      },
+      {
+        "featureType": "landscape.natural.landcover",
+        "stylers": [
+          { "hue": "#eb7b59" },
+          { "saturation": 0 },
+          { "lightness": 0 }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "stylers": [
+          { "visibility": "on" }
+        ]
+      },
+      {
+        "featureType": "administrative.neighborhood",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "stylers": [
+          { "hue": "#eb7b59"},
+          { "visibility": "simplified" },
+          { "saturation": 70 },
+          { "lightness": 10 }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      }
+    ];
+
+    this.mapOptions = {
+      zoom: 4,
+      center: new google.maps.LatLng(40.6,-95.665),
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, "custom_style"]
+      },
+      mapTypeId: "custom_style",
+      disableDefaultUI: true
+    };
+
+    this.map = new google.maps.Map(this.$el.get(0), this.mapOptions);
+    this.customMapType = new google.maps.StyledMapType(this.featureOpts, { name: "custom_style" });
+    this.map.mapTypes.set("custom_style", this.customMapType);
+  }
+});
